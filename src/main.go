f7754c736d9cfc14911794e72caa86e369331ecf
Victor Moraes <talkto@moraesvic.com>
2023-12-29 23:34:59 -0300

    works perfectly

--- src/main.go
+++ src/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
+	"io"
 	"log"
+	"os"
 	"os/exec"
 	"strings"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/polly"
@@ -13,7 +17,11 @@ import (
 	"github.com/mozillazg/go-pinyin"
 )
 
-func toPinyin(s string) string {
+const (
+	TEMPO = 0.7
+)
+
+func _toPinyin(s string) string {
 	pinyinArgs := pinyin.NewArgs()
 	pinyinArgs.Style = pinyin.Tone
 
@@ -21,14 +29,19 @@ func toPinyin(s string) string {
 	return strings.Join(result, " ")
 }
 
-func changeAudioTempo(path string, tempo float32) string {
+func toPinyin(ch chan Sentence, sentence Sentence) {
+	sentence.textTransliterated = _toPinyin(sentence.textOriginal)
+	ch <- sentence
+}
+
+func _changeAudioTempo(path string, tempo float64) string {
 	if !strings.HasSuffix(path, ".mp3") {
 		panic("Only files of MP3 type are allowed!")
 	}
 
 	executable := "ffmpeg"
-	inputFile := path
-	atempo := fmt.Sprintf("atempo=%f", tempo)
+	inputFile := fmt.Sprintf("file:%s", path)
+	atempo := fmt.Sprintf("atempo=%.2f", tempo)
 	// For quality, lower is better
 	_quality := 2
 	quality := fmt.Sprint(_quality)
@@ -44,44 +57,155 @@ func changeAudioTempo(path string, tempo float32) string {
 		atempo,
 		"-q:a",
 		quality,
-		outputPath)
+		fmt.Sprintf("file:%s", outputPath))
 
-	out, err := cmd.CombinedOutput()
+	res, err := cmd.CombinedOutput()
 	if err != nil {
+		fmt.Printf("Error processing %s into %s", path, outputPath)
+		fmt.Print(string(res))
+		fmt.Print()
 		log.Fatal(err)
 	}
-	fmt.Printf("%s\n", out)
 
 	return outputPath
 }
 
-func main() {
-	fmt.Println("Starting program...")
+func changeAudioTempo(ch chan Sentence, sentence Sentence) {
+	sentence.audioReducedSpeed = _changeAudioTempo(sentence.audioOriginal, TEMPO)
+	ch <- sentence
+}
 
-	chinese := "我说中文。我女朋友叫"
-	fmt.Println(toPinyin(chinese))
+func synthesizeSpeech(ch chan Sentence, sentence Sentence, client *polly.Client) {
+	params := &polly.SynthesizeSpeechInput{
+		Engine:       types.EngineNeural,
+		OutputFormat: types.OutputFormatMp3,
+		Text:         &sentence.textOriginal,
+		VoiceId:      types.VoiceIdZhiyu,
+	}
 
+	res, err := client.SynthesizeSpeech(context.TODO(), params)
+	if err != nil {
+		panic("Could not synthesize speech!")
+	}
+
+	audio, err := io.ReadAll(res.AudioStream)
+	if err != nil {
+		panic("Could not read audio stream!")
+	}
+
+	audioOriginal := fmt.Sprintf("%s.mp3", sentence.id)
+
+	err = os.WriteFile(audioOriginal, audio, 0644)
+	if err != nil {
+		panic(fmt.Sprintf("Error writing audio data to file: %s", err))
+	}
+
+	sentence.audioOriginal = audioOriginal
+	// fmt.Printf("Original audio extracted to %s\n", audioOriginal)
+	ch <- sentence
+}
+
+type Sentence struct {
+	id                 string
+	textOriginal       string
+	textTransliterated string
+	audioOriginal      string
+	audioReducedSpeed  string
+}
+
+func printSentence(sentence Sentence) {
+	fmt.Printf("%-20s : %-20s\n", "ID", sentence.id)
+	fmt.Printf("%-20s : %-20s\n", "Original text", sentence.textOriginal)
+	fmt.Printf("%-20s : %-20s\n", "Transliterated text", sentence.textTransliterated)
+	fmt.Printf("%-20s : %-20s\n", "Original audio", sentence.audioOriginal)
+	fmt.Printf("%-20s : %-20s\n", "Reduced speed audio", sentence.audioReducedSpeed)
+	fmt.Println()
+}
+
+func makeId(timestamp string, index int) string {
+	return fmt.Sprintf("%s-%04d", timestamp, index)
+}
+
+func makeSentence(ch chan Sentence, timestamp string, index int, textOriginal string) {
+	sentence := Sentence{}
+	sentence.id = makeId(timestamp, index)
+	sentence.textOriginal = textOriginal
+	ch <- sentence
+}
+
+func getLines() []string {
+	filePath := os.Args[1]
+
+	// Open the file
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+
+	// Read lines from the file and store them in the slice
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+
+	return lines
+}
+
+func getClient() *polly.Client {
 	cfg, err := config.LoadDefaultConfig(context.TODO())
 	if err != nil {
 		panic("Cannot load AWS config!")
 	}
 
 	client := polly.NewFromConfig(cfg)
-	fmt.Println(client)
+	return client
+}
 
-	// Create input parameters for the SynthesizeSpeech operation
-	params := &polly.SynthesizeSpeechInput{
-		Engine:       types.EngineNeural,
-		OutputFormat: types.OutputFormatMp3,
-		Text:         &chinese,
-		VoiceId:      types.VoiceIdZhiyu,
+func main() {
+	timestamp := time.Now().Format(time.RFC3339)
+	fmt.Printf("Starting program at %s\n", timestamp)
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <file_path>")
+		os.Exit(1)
 	}
 
-	res, err := client.SynthesizeSpeech(context.TODO(), params)
-	if err != nil {
-		panic(err)
+	client := getClient()
+	lines := getLines()
+
+	// for idx, line := range lines {
+	// 	runLineSequential(timestamp, idx, &line, client)
+	// }
+
+	pipe1 := make(chan Sentence, len(lines))
+	pipe2 := make(chan Sentence, len(lines))
+	pipe3 := make(chan Sentence, len(lines))
+	pipe4 := make(chan Sentence, len(lines))
+
+	for index, text := range lines {
+		go makeSentence(pipe1, timestamp, index, text)
 	}
-	fmt.Println(res)
+
+	for range lines {
+		go synthesizeSpeech(pipe2, <-pipe1, client)
+	}
+
+	for range lines {
+		go changeAudioTempo(pipe3, <-pipe2)
+	}
+
+	for range lines {
+		go toPinyin(pipe4, <-pipe3)
+	}
+
+	for range lines {
+		printSentence(<-pipe4)
+	}
 
 	// changeAudioTempo("hello.mp3", 0.75)
 }
